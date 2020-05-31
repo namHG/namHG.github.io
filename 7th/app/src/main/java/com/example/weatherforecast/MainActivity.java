e2be6465a68aee40e604fd8525a36a2611e210ac
namHG <onep5286@gmail.com>
2020-05-31 19:04:26 +0900

    7th_5

--- 7th/app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ 7th/app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -16,11 +16,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
